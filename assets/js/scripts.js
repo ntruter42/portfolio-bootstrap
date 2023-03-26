95417a4ae66bbcffcb52128b3f3b18701fc8f380
Nicholas Truter <ntruter42@gmail.com>
2023-03-27 00:11:35 +0200

    Add reset.css, Update index.html, style.css - trying new navbar with rotating feature

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,55 +1,54 @@
-// NAVBAR CURRENT SECTION HIGHLIGHT
-window.addEventListener('scroll', function() {
-	var section1 = document.getElementById('intro-section');
-	var section1Top = section1.getBoundingClientRect().top;
+// window.addEventListener('scroll', function() {
+// 	var section = document.getElementById('intro-section');
+// 	var sectionTop = section.getBoundingClientRect().top;
 	
-	if (section1Top < 60 && section1Top > -section1.offsetHeight) {
-	  document.querySelector('.nav-link-1').classList.add('active');
-	} else {
-	  document.querySelector('.nav-link-1').classList.remove('active');
-	}
-});
+// 	if (section < 60 && sectionTop > -section.offsetHeight) {
+// 	  document.querySelector('.nav-link-1').classList.add('active');
+// 	} else {
+// 	  document.querySelector('.nav-link-1').classList.remove('active');
+// 	}
+// });
 
-window.addEventListener('scroll', function() {
-	var section1 = document.getElementById('skills-section');
-	var section1Top = section1.getBoundingClientRect().top;
+// window.addEventListener('scroll', function() {
+// 	var section = document.getElementById('skills-section');
+// 	var sectionTop = section.getBoundingClientRect().top;
 	
-	if (section1Top < 60 && section1Top > -section1.offsetHeight) {
-	  document.querySelector('.nav-link-2').classList.add('active');
-	} else {
-	  document.querySelector('.nav-link-2').classList.remove('active');
-	}
-});
+// 	if (sectionTop < 60 && sectionTop > -section.offsetHeight) {
+// 	  document.querySelector('.nav-link-2').classList.add('active');
+// 	} else {
+// 	  document.querySelector('.nav-link-2').classList.remove('active');
+// 	}
+// });
 
-window.addEventListener('scroll', function() {
-	var section1 = document.getElementById('projects-section');
-	var section1Top = section1.getBoundingClientRect().top;
+// window.addEventListener('scroll', function() {
+// 	var section = document.getElementById('projects-section');
+// 	var sectionTop = section.getBoundingClientRect().top;
 	
-	if (section1Top < 60 && section1Top > -section1.offsetHeight) {
-	  document.querySelector('.nav-link-3').classList.add('active');
-	} else {
-	  document.querySelector('.nav-link-3').classList.remove('active');
-	}
-});
+// 	if (sectionTop < 60 && sectionTop > -section.offsetHeight) {
+// 	  document.querySelector('.nav-link-3').classList.add('active');
+// 	} else {
+// 	  document.querySelector('.nav-link-3').classList.remove('active');
+// 	}
+// });
 
-window.addEventListener('scroll', function() {
-	var section1 = document.getElementById('contact-section');
-	var section1Top = section1.getBoundingClientRect().top;
+// window.addEventListener('scroll', function() {
+// 	var section = document.getElementById('contact-section');
+// 	var sectionTop = section.getBoundingClientRect().top;
 	
-	if (section1Top < 60 && section1Top > -section1.offsetHeight) {
-	  document.querySelector('.nav-link-4').classList.add('active');
-	} else {
-	  document.querySelector('.nav-link-4').classList.remove('active');
-	}
-});
+// 	if (sectionTop < 60 && sectionTop > -section.offsetHeight) {
+// 	  document.querySelector('.nav-link-4').classList.add('active');
+// 	} else {
+// 	  document.querySelector('.nav-link-4').classList.remove('active');
+// 	}
+// });
 
-window.addEventListener('scroll', function() {
-	var section1 = document.getElementById('social-section');
-	var section1Top = section1.getBoundingClientRect().top;
+// window.addEventListener('scroll', function() {
+// 	var section = document.getElementById('social-section');
+// 	var sectionTop = section.getBoundingClientRect().top;
 	
-	if (section1Top < 60 && section1Top > -section1.offsetHeight) {
-	  document.querySelector('.nav-link-5').classList.add('active');
-	} else {
-	  document.querySelector('.nav-link-5').classList.remove('active');
-	}
-});
+// 	if (sectionTop < 60 && sectionTop > -section.offsetHeight) {
+// 	  document.querySelector('.nav-link-5').classList.add('active');
+// 	} else {
+// 	  document.querySelector('.nav-link-5').classList.remove('active');
+// 	}
+// });
